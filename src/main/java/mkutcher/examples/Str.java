de4c9e734fcf16a0dd9cdaed0a35255dec27c2c0
v.novolotskiy <chaser10101>
2022-05-20 10:57:59 +0300

    add examples

---
+++ src/main/java/mkutcher/examples/Str.java
@@ -0,0 +1,9 @@
+package main.java.mkutcher.examples;
+
+public class Str {
+    public static void main(String[] args) {
+        String string = "Hello";
+        string.concat(" World");
+        System.out.println(string);
+    }
+}
